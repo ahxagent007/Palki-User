07341cb886f5bf40cb2a700ba7a527e1f2dad0f9
AHXagent007 <ahx.agent007@gmail.com>
2022-08-03 23:59:08 +0600

    Help page design and MAP API done

--- app/src/main/java/com/alphacuetech/xian/palki_drive/ui/help/HelpFragment.java
+++ app/src/main/java/com/alphacuetech/xian/palki_drive/ui/help/HelpFragment.java
@@ -24,8 +24,8 @@ private FragmentHelpBinding binding;
     binding = FragmentHelpBinding.inflate(inflater, container, false);
     View root = binding.getRoot();
 
-        final TextView textView = binding.textHelp;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        /*final TextView textView = binding.textHelp;
+        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);*/
         return root;
     }
 
