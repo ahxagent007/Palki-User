8193ef809c490047fc9ffb25f48962935f2fa40d
AHXagent007 <ahx.agent007@gmail.com>
2022-08-12 03:25:58 +0600

    place api implemented

---
+++ app/src/main/java/com/alphacuetech/xian/palki_drive/Activities/MapsActivity.java
@@ -0,0 +1,101 @@
+package com.alphacuetech.xian.palki_drive.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.alphacuetech.xian.palki_drive.R;
+import com.alphacuetech.xian.palki_drive.databinding.ActivityMapsBinding;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback  {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    String TAG = "XIAN";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), getString(R.string.maps_api_key), Locale.US);
+        }
+
+        // Initialize the AutocompleteSupportFragment.
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull Place place) {
+
+                LatLng selectedLatLong = place.getLatLng();
+
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "onPlaceSelected Place: " + place.getName() + ", " + place.getId()+", LAT LONG "+ selectedLatLong.toString());
+
+                mMap.addMarker(new MarkerOptions().position(selectedLatLong).title(place.getName()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(selectedLatLong));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(selectedLatLong, 16f));
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng testLatLng = new LatLng(-34, 151);
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = testLatLng;
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(testLatLng, 15f));
+    }
+
+
+
+
+}
--- app/src/main/java/com/alphacuetech/xian/palki_drive/Activities/TripActivity.java
+++ app/src/main/java/com/alphacuetech/xian/palki_drive/Activities/TripActivity.java
@@ -2,15 +2,35 @@ package com.alphacuetech.xian.palki_drive.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.alphacuetech.xian.palki_drive.R;
 
 public class TripActivity extends AppCompatActivity {
 
+    LinearLayout LL_sedan, LL_premium_sedan, LL_mini_microbus, LL_microbus, LL_minibus, LL_bike;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trip);
+
+        LL_sedan = findViewById(R.id.LL_sedan);
+        LL_premium_sedan = findViewById(R.id.LL_premium_sedan);
+        LL_mini_microbus = findViewById(R.id.LL_mini_microbus);
+        LL_minibus = findViewById(R.id.LL_minibus);
+        LL_microbus = findViewById(R.id.LL_microbus);
+        LL_bike = findViewById(R.id.LL_bike);
+
+        LL_sedan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+            }
+        });
+
     }
 }
